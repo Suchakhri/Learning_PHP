4e58190dfe76582e00ab3d7b6feaede66d5bf01c
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-03 11:24:53 +0700

    1st PHP

--- api/index.php
+++ api/index.php
@@ -7,9 +7,19 @@
     <title>PHP on Vercel</title>
 </head>
 <body>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
+<h3>GET_METHOD</h3>
+<form action="get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+<br />
+<br />
+<h3>POST_METHOD</h3>
+<form action="get.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
 </form>
 
 <a href="test_get.php?subject=PHP&web=W3schools.com">Test $GET</a>
@@ -17,17 +27,9 @@
 //  This is comment.
 #   This is comment.
 /*  This is Comment.   */ 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_POST['fname'];
-    if (empty($name)) {
-      echo "Name is empty";
-    } else {
-      echo $name;
-    }
-  }
 
-echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+
+
 
 ?>
 

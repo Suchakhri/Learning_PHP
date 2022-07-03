011d4db397d2f3862b3d5fb92ab3dec8d49828cd
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-03 10:47:03 +0700

    1st PHP

--- api/index.php
+++ api/index.php
@@ -13,11 +13,11 @@
 /*  This is Comment.   */ 
 $x = 5; // global scope
 
-function myTest(x) {
+function myTest(){
   // using x inside this function will generate an error
   echo "<p>Variable x inside function is: $x </p>";
 }
-myTest(x);
+myTest();
 
 echo "<p>Variable x outside function is: $x</p>";
 ?>

535600ca6f60ef087c62685fb0eb30b6897b45e7
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-03 10:50:58 +0700

    1st PHP

--- api/index.php
+++ api/index.php
@@ -11,15 +11,23 @@
 //  This is comment.
 #   This is comment.
 /*  This is Comment.   */ 
-global $x = 5; // global scope
-
-function myTest(){
-  // using x inside this function will generate an error
-  echo "<p>Variable x inside function is: $x </p>";
-}
-myTest();
-
-echo "<p>Variable x outside function is: $x</p>";
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("black", "Volvo");
+  echo $myCar -> message();
+  echo "<br>";
+  $myCar = new Car("red", "Toyota");
+  echo $myCar -> message();
 ?>
 
 </body>
